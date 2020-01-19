5a1465c5da85bfeeac6c722a9faa06d73e7c0bae
Álvaro Vilanova Vidal <alvivi@gmail.com>
2020-01-19 11:15:44 +0100

    Add basic development libraries
    Basic development libraries include ex_doc, dialyxir, propcheck and
    excoveralls.

--- mix.exs
+++ mix.exs
@@ -7,22 +7,44 @@ defmodule Shop.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer(),
+      test_coverage: test_coverage(),
+      preferred_cli_env: preferred_cli_env()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:propcheck, "~> 1.2", only: [:test, :dev]}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp test_coverage do
+    [
+      tool: ExCoveralls
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      "coveralls.detail": :test,
+      "coveralls.html": :test,
+      "coveralls.json": :test,
+      coveralls: :test
     ]
   end
 end
